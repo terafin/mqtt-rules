ceee9893b5fa4eb9b6d8ccf9e78293dd97ef492d
Justin Wood <woody@apple.com>
2017-05-10 01:02:53 -0700

    Squashed 'homeautomation-js-lib/' changes from 5850e57..dafc366
    dafc366 Updating
    75fe038 Handle user/pass
    e1dbc6b adding redis helpers
    2c8c6d1 Updating airscape
    f6e1812 Cleaning up helpers
    6943b76 Fixing
    
    git-subtree-dir: homeautomation-js-lib
    git-subtree-split: dafc366e480e51c5cc35960a674860f931e730aa

--- homeseer.js
+++ homeseer.js
@@ -1,4 +1,5 @@
 const logging = require('./logging.js')
+const _ = require('lodash')
 const request = require('request')
 var homeseer_json_api_path = null
 
@@ -7,7 +8,7 @@ exports.set_path = function(new_path) {
 }
 
 exports.publish = function(deviceRefID, targetValue) {
-    if (homeseer_json_api_path === null || homeseer_json_api_path === undefined) {
+    if (_.isNil(homeseer_json_api_path)) {
         logging.error('homeseer_json_api_path not defined')
         return
     }
@@ -20,7 +21,12 @@ exports.publish = function(deviceRefID, targetValue) {
         url: homeseer_url
     })
     request(homeseer_url, function(error, response, body) {
-        if ((response !== null) || (error !== null && error !== undefined)) {
+        var statusCode = 401
+
+        if (response !== null && response.statusCode !== undefined) {
+            statusCode = response.statusCode
+        }
+        if (statusCode !== 200 && (!_.isNil(response) || !_.isNil(error))) {
             logging.error('homeseer action failed: ' + homeseer_url, {
                 event: 'homeseer-action-failed',
                 error: error,
