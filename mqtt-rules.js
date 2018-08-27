01f647c886855352d3df2b690e9a48035ca7f59e
Justin Wood <woody@apple.com>
2018-08-27 09:39:46 -0700

    Fixing nil checks

--- mqtt-rules.js
+++ mqtt-rules.js
@@ -435,11 +435,12 @@ rules.on('rules-loaded', () => {
 
 	logging.info('rules loaded')
 	rules.get_configs().forEach(rule => {
-		const keys = Object.keys(rule)
 		if ( _.isNil(rule) ) { 
 			logging.error(' empty rule from iterator')
 			return 
 		}
+
+		const keys = Object.keys(rule)
 
 		if ( _.isNil(keys) ) { 
 			logging.error(' missing keys from rule: ' + JSON.stringify(rule))
