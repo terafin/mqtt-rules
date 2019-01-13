0e221d83b34c37a09cd5444874e93c2447000f69
terafin <jwood@me.com>
2019-01-13 00:02:29 -0800

    Logging for options correctly

--- mqtt-rules.js
+++ mqtt-rules.js
@@ -213,11 +213,11 @@ global.publish = function(rule_name, expression, valueOrExpression, topic, messa
 	}
 
 	if (!quiet && !global.isTestMode()) {
-		logging.info('=> rule: ' + rule_name + '  publishing: ' + topic + ':' + message + ' (expression: ' + expression + ' | value: ' + valueOrExpression + ')' + '  options: ' + JSON.stringify(inOptions))
+		logging.info('=> rule: ' + rule_name + '  publishing: ' + topic + ':' + message + ' (expression: ' + expression + ' | value: ' + valueOrExpression + ')' + '  options: ' + JSON.stringify(options))
 	}
 
 	if (_.isNil(global.client)) {
-		logging.error('=> (client not initialized, not publishing) rule: ' + rule_name + '  publishing: ' + topic + ':' + message + ' (expression: ' + expression + ' | value: ' + valueOrExpression + ')' + '  options: ' + JSON.stringify(inOptions))
+		logging.error('=> (client not initialized, not publishing) rule: ' + rule_name + '  publishing: ' + topic + ':' + message + ' (expression: ' + expression + ' | value: ' + valueOrExpression + ')' + '  options: ' + JSON.stringify(options))
 	} else {
 
 
